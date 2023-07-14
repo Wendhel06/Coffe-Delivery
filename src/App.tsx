062229a9d2f6f1e87d3aa5d5fd24e92d2585b452
Wendhel Alves Nogueira <wendhelalves7@gmail.com>
2023-07-13 21:16:19 -0300

    Begining the project

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <h1>Tudo funcionando</h1>
+    <GlobalStyle />;
+  )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+
+h1,h2,h3,h4,h5 {
+  font-family: "Ysabeau", sans-serif;
+}
+
+`
